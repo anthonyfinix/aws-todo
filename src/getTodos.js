ec1d4edfec72173bcbc9a2b9303b2026b1c3e7a3
Anthony Finix <anthonyfinix@gmail.com>
2026-03-28 17:05:06 +0530

    initial commit

---
+++ src/getTodos.js
@@ -0,0 +1,26 @@
+// Use require for plain JavaScript (CommonJS)
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, ScanCommand } = require("@aws-sdk/lib-dynamodb");
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+exports.handler = async (event) => {
+    try {
+        const params = {
+            TableName: process.env.TABLE_NAME
+        };
+
+        const command = new ScanCommand(params);
+        const data = await docClient.send(command);
+
+        return {
+            statusCode: 200,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data.Items),
+        };
+    } catch (err) {
+        console.error(err);
+        return { statusCode: 500, body: JSON.stringify({ message: "Error" }) };
+    }
+};
